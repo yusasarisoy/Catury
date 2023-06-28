1c17b6a73956a8d38420ded82878a4dbd7fb36ab
Yuşa  <yusasarisoy@icloud.com>
2023-06-28 21:46:45 +0200

    Initial Commit

---
+++ Catury/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Catury
+//
+//  Created by Yuşa on 28.06.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
