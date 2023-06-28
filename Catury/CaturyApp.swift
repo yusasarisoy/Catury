934bf0533b7c690862cc8734d3a3475b6ccac8a1
Yuşa  <yusasarisoy@icloud.com>
2023-06-28 21:48:24 +0200

    Initialize commit

--- Catury/CaturyApp.swift
+++ Catury/CaturyApp.swift
@@ -1,17 +1,10 @@
-//
-//  CaturyApp.swift
-//  Catury
-//
-//  Created by Yuşa on 28.06.23.
-//
-
 import SwiftUI
 
 @main
 struct CaturyApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
